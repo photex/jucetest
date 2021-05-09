4a6df206100ed5898910781aef3d464a18e2c41b
Chip Collier <photex@gmail.com>
2021-05-09 20:13:46 +0200

    setup of clang-tidy

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 
 #include <iostream>
+#include <juce_core/juce_core.h>
 
 int main(int argc, char *argv[]) {
+  juce::ignoreUnused(argc, argv);
+
   std::cout << "Hello, JUCE" << std::endl;
+
   return 0;
 }
